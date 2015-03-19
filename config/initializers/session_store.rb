8a2cbb1df655eb850f461e7a95911da8755bd371
Juan Pablo Amaya <jp.amaya@codescrum.com>
2015-03-19 16:43:31 -0500

    Change session store to redis

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_railstemplate_session'
+Rails.application.config.session_store :redis_store, redis_server: "redis://127.0.0.1:6379/0/_sso_rails4_example_app_session"
